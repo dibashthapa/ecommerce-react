38190cabb524969d1c50b1b382a7305cf9685e10
Dibash Thapa <dibashthapa55@gmail.com>
2021-01-28 22:29:21 +0545

    fixed changes

--- src/Public/pages/Login/index.js
+++ src/Public/pages/Login/index.js
@@ -1,96 +1,61 @@
 import React, { useState } from 'react';
-
-import { InputLabel, makeStyles } from '@material-ui/core';
-
-import Box from '../../UI/Login-SignupBox/Box';
+import { Grid, InputLabel, TextField } from '@material-ui/core';
 import MuiButton from '../../UI/Button/Button';
-import MuiInput from '../../UI/Input/Input';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    position: 'relative',
-  },
-  fonts: {
-    fontSize: theme.overrides.s6Default.fontSize,
-    fontWeight: theme.overrides.s6Default.fontWeight,
-    lineHeight: theme.overrides.s6Default.lineHeight,
-    color: 'black',
-    display: 'inline',
-  },
-  left: {
-    fontSize: theme.overrides.s6Default.fontSize,
-    fontWeight: theme.overrides.s6Default.fontWeight,
-    lineHeight: theme.overrides.s6Default.lineHeight,
-    color: '#9C9C9C',
-    position: 'absolute',
-    display: 'inline',
-    right: 0,
-    textDecoration: 'underline',
-    cursor: 'pointer',
-  },
-  link: {
-    textDecoration: 'underline',
-    cursor: 'pointer',
-    fontWeight: 'bold',
-  },
-}));
-
+import useStyles from './index.style';
 const Login = () => {
-  let initialState = {
-    email: '',
-    password: '',
-  };
+  const [values, setValue] = useState('');
 
-  const [form, setForm] = useState(initialState);
-
-  const onChange = e => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    setForm({
-      ...form,
+    setValue({
+      ...values,
       [name]: value,
     });
   };
 
   const classes = useStyles();
   return (
-    <Box>
-      <form className={classes.root}>
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      className={classes.loginContainer}
+    >
+      <Grid xs={12} lg={3} item>
         <InputLabel className={classes.fonts} htmlFor="email">
           Email
         </InputLabel>
-        <MuiInput
-          changed={onChange}
-          value={form.email}
-          className={classes.fonts}
-          fullWidth
-          placeholder="email"
+        <TextField
+          onChange={handleChange}
+          name={'email'}
+          value={values.email}
           type="email"
-          name="email"
+          className={classes.input}
+          fullWidth
+          variant={'outlined'}
         />
-        <br />
         <InputLabel className={classes.fonts} htmlFor="password">
           Password
         </InputLabel>
         <InputLabel className={classes.left} htmlFor="password">
           forgot password?
         </InputLabel>
-        <MuiInput
-          changed={onChange}
-          value={form.password}
-          className={classes.fonts}
+        <TextField
+          value={values.password}
+          onChange={handleChange}
+          name={'password'}
+          autoComplete="off"
+          className={classes.input}
           fullWidth
-          placeholder="password"
           type="password"
-          name="password"
+          variant={'outlined'}
         />
         <MuiButton type="submit">Submit</MuiButton>
-        <br />
-        <br />
         <InputLabel htmlFor="password">
           Don't have an account? <span className={classes.link}>Create</span>
         </InputLabel>
-      </form>
-    </Box>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/Public/pages/Login/index.style.js
+++ src/Public/pages/Login/index.style.js
@@ -1,9 +1,38 @@
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    root :{
-        fontSize:15
-    }
-}))
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: 'relative',
+  },
+  loginContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50% , -50%)',
+  },
+  fonts: {
+    fontSize: theme.overrides.s6Default.fontSize,
+    fontWeight: theme.overrides.s6Default.fontWeight,
+    lineHeight: theme.overrides.s6Default.lineHeight,
+    color: 'black',
+    display: 'inline',
+  },
+  left: {
+    fontSize: theme.overrides.s6Default.fontSize,
+    fontWeight: theme.overrides.s6Default.fontWeight,
+    lineHeight: theme.overrides.s6Default.lineHeight,
+    color: '#9C9C9C',
+    position: 'absolute',
+    display: 'inline',
+    right: 0,
+    textDecoration: 'underline',
+    cursor: 'pointer',
+  },
+  link: {
+    textDecoration: 'underline',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+  },
+}));
 
-export default useStyles
+export default useStyles;
