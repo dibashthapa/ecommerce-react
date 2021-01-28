9c87f0b92a8fcfc5390ab20a821986b605a60fe3
Dibash Thapa <dibashthapa55@gmail.com>
2021-01-28 22:48:42 +0545

    fixed login ui

--- src/Public/UI/Button/Button.js
+++ src/Public/UI/Button/Button.js
@@ -2,29 +2,22 @@ import React from 'react';
 
 import { Button, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        margin: `${theme.spacing(2)}px 0`,
-        color: 'white',
-        fontWeight: '900',
-        textTransform: 'none',
-    }
-}))
+const useStyles = makeStyles(theme => ({}));
 
 function MuiButton(props) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Button
-            className={classes.root}
-            variant={props.variant || 'contained'}
-            size={props.size || 'small'}
-            color={props.color || 'primary'}
-            type={props.type || 'button'}
-        >
-            {props.children}
-        </Button>
-    )
+  return (
+    <Button
+      className={classes.root}
+      variant={props.variant || 'contained'}
+      size={props.size || 'small'}
+      color={props.color || 'primary'}
+      type={props.type || 'button'}
+    >
+      {props.children}
+    </Button>
+  );
 }
 
-export default MuiButton
+export default MuiButton;
--- src/Public/pages/Login/index.js
+++ src/Public/pages/Login/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import { Grid, InputLabel, TextField } from '@material-ui/core';
-import MuiButton from '../../UI/Button/Button';
+import {
+  Button,
+  Grid,
+  InputLabel,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import useStyles from './index.style';
 const Login = () => {
   const [values, setValue] = useState('');
@@ -21,39 +26,55 @@ const Login = () => {
       alignItems="center"
       className={classes.loginContainer}
     >
-      <Grid xs={12} lg={3} item>
-        <InputLabel className={classes.fonts} htmlFor="email">
-          Email
-        </InputLabel>
-        <TextField
-          onChange={handleChange}
-          name={'email'}
-          value={values.email}
-          type="email"
-          className={classes.input}
-          fullWidth
-          variant={'outlined'}
-        />
-        <InputLabel className={classes.fonts} htmlFor="password">
-          Password
-        </InputLabel>
-        <InputLabel className={classes.left} htmlFor="password">
+      <Grid xs={12} lg={3} item className="loginForm">
+        <div className="emailSection">
+          <InputLabel className={classes.fonts} htmlFor="email">
+            Email
+          </InputLabel>
+          <TextField
+            onChange={handleChange}
+            name={'email'}
+            value={values.email}
+            type="email"
+            size="small"
+            className={classes.input}
+            fullWidth
+            variant={'outlined'}
+          />
+        </div>
+        <Typography className={classes.left} htmlFor="password">
           forgot password?
-        </InputLabel>
-        <TextField
-          value={values.password}
-          onChange={handleChange}
-          name={'password'}
-          autoComplete="off"
-          className={classes.input}
-          fullWidth
-          type="password"
-          variant={'outlined'}
-        />
-        <MuiButton type="submit">Submit</MuiButton>
-        <InputLabel htmlFor="password">
+        </Typography>
+        <div className="passwordSection">
+          <InputLabel className={classes.fonts} htmlFor="password">
+            Password
+          </InputLabel>
+
+          <TextField
+            value={values.password}
+            onChange={handleChange}
+            name={'password'}
+            autoComplete="off"
+            className={classes.input}
+            fullWidth
+            size="small"
+            type="password"
+            variant={'outlined'}
+          />
+        </div>
+
+        <Button
+          className={classes.loginButton}
+          variant={'contained'}
+          size={'small'}
+          color={'primary'}
+          type={'submit'}
+        >
+          Login
+        </Button>
+        <Typography>
           Don't have an account? <span className={classes.link}>Create</span>
-        </InputLabel>
+        </Typography>
       </Grid>
     </Grid>
   );
