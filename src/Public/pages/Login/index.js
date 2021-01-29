f18127d8eb6d51c7501467a2390ebea1051850d9
Saurab Gami <saurabgami977@gmail.com>
2021-01-29 22:33:17 +0545

    show/hide password added in login form

--- src/Public/pages/Login/index.js
+++ src/Public/pages/Login/index.js
@@ -2,13 +2,24 @@ import React, { useState } from 'react';
 import {
   Button,
   Grid,
+  IconButton,
+  InputAdornment,
   InputLabel,
   TextField,
   Typography,
 } from '@material-ui/core';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import useStyles from './index.style';
+
 const Login = () => {
-  const [values, setValue] = useState('');
+
+  const [values, setValue] = useState({
+    email: '',
+    password: '',
+    showPassword: false
+  });
+
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -27,11 +38,13 @@ const Login = () => {
       className={classes.loginContainer}
     >
       <Grid xs={12} lg={3} item className="loginForm">
-        <div className="emailSection">
+        <form noValidate><div className="emailSection">
           <InputLabel className={classes.fonts} htmlFor="email">
             Email
-          </InputLabel>
+        </InputLabel>
           <TextField
+            // error={!isEmailValid}
+            // helperText={!isEmailValid ? "Email is not valid" : null}
             onChange={handleChange}
             name={'email'}
             value={values.email}
@@ -42,35 +55,48 @@ const Login = () => {
             variant={'outlined'}
           />
         </div>
-        <Typography className={classes.left} htmlFor="password">
-          forgot password?
+          <Typography className={classes.left} htmlFor="password">
+            forgot password?
         </Typography>
-        <div className="passwordSection">
-          <InputLabel className={classes.fonts} htmlFor="password">
-            Password
-          </InputLabel>
+          <div className="passwordSection">
+            <InputLabel className={classes.fonts} htmlFor="password">
+              Password
+        </InputLabel>
 
-          <TextField
-            value={values.password}
-            onChange={handleChange}
-            name={'password'}
-            className={classes.input}
-            fullWidth
-            size="small"
-            type="password"
-            variant={'outlined'}
-          />
-        </div>
+            <TextField
+              // error={!isPasswordValid}
+              // helperText={!isPasswordValid ? "Password must be greater than 8 character" : null}
+              value={values.password}
+              InputProps={{
+                endAdornment: <InputAdornment position="end" >
+                  <IconButton
+                    onClick={() => setValue({ ...values, showPassword: !values.showPassword })}
+                  >
+                    {values.showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }}
+              onChange={handleChange}
+              name={'password'}
+              className={classes.input}
+              fullWidth
+              size="small"
+              type={values.showPassword ? 'text' : 'password'}
+              variant='outlined'
+            />
+          </div>
 
-        <Button
-          className={classes.loginButton}
-          variant={'contained'}
-          size={'small'}
-          color={'primary'}
-          type={'submit'}
-        >
-          Login
+          <Button
+            // disabled={isPasswordValid && isEmailValid ? false : true}
+            className={classes.loginButton}
+            variant={'contained'}
+            size={'small'}
+            color={'primary'}
+            type={'submit'}
+          >
+            Login
         </Button>
+        </form>
         <Typography>
           Don't have an account? <span className={classes.link}>Create</span>
         </Typography>
