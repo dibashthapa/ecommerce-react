19a20f0dae395a58da24ee5b70694f33d46ea661
Saurab Gami <saurabgami977@gmail.com>
2021-02-02 12:25:00 +0545

    product ui completed

--- src/App/Components/Product/Product.js
+++ src/App/Components/Product/Product.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { Button, Card, CardMedia, Typography } from '@material-ui/core';
 
 import useStyles from './Product.style'
 
 const productDetail = {
     title: 'Product Name',
-    description: 'this is long long long long long long long long long long long long long long long long long long long long long long Description',
+    description: 'This is long long long long long long long long long long long long long long long long long long long long long long Description',
     price: 12.3,
-    discount_percentage: 3,
-    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiqUTXr9s1eGYhjPfmVNIdOmp-kvZc9fXMCg&usqp=CAU"
+    discount_percentage: 40,
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiqUTXr9s1eGYhjPfmVNIdOmp-kvZc9fXMCg&usqp=CAU",
+    previous_price: 40
 }
 
 function Product() {
@@ -20,23 +21,27 @@ function Product() {
                 image={productDetail.image_url}
                 title={productDetail.title}
             />
-            <CardContent>
-                <Typography variant="h5" component="h2">
-                    {productDetail.title}
+            <Typography className={classes.discountPercentage}>
+                {'-' + productDetail.discount_percentage + '%'}
+            </Typography>
+            <Typography className={classes.title}>
+                {productDetail.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                {productDetail.description.substring(0, 30) + '...'}
+            </Typography>
+            <div className={classes.actions}>
+                <Typography className={classes.price}>
+                    {productDetail.price + ' USD'}
                 </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {productDetail.description.substring(0, 30)}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Typography>
-                    {productDetail.price + ' $'}
-                </Typography>
-                <Button size="small" color="primary" variant="contained">
+                <Button size="small" color="primary" variant="contained" className={classes.button}>
                     Buy now
                 </Button>
-            </CardActions>
-        </Card>
+            </div>
+            <Typography variant="body2" color="textSecondary" className={classes.previousPrice}>
+                {productDetail.previous_price + ' $'}
+            </Typography>
+        </Card >
     )
 }
 
--- src/App/Components/Product/Product.style.js
+++ src/App/Components/Product/Product.style.js
@@ -1,17 +1,52 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     root: {
         maxWidth: 270,
         border: '1px solid #D1D1D1',
         borderRadius: '12px',
-        padding: '16px'
-
+        padding: '16px',
+        position: 'relative',
     },
     media: {
         height: 180,
+    },
+    discountPercentage: {
+        position: 'absolute',
+        top: '0%',
+        fontWeight: 700,
+        fontSize: '12px',
+        lineHeight: '18px',
+        margin: '10px 0px 0px 10px',
+        color: theme.palette.primary.main
+    },
+    title: {
+        fontSize: '15px',
+        fontWeight: 700,
+        lineHeight: '23px',
+        letterSpacing: '0em',
+        textAlign: 'left',
+
+    },
+    actions: {
+        display: 'flex',
+        justifyContent: "space-between",
+        paddingTop: '12px',
+    },
+    previousPrice: {
+        textDecoration: 'line-through'
+    },
+    price: {
+        fontWeight: 700,
+        textAlign: 'left'
+    },
+    button: {
+        color: theme.palette.secondary.main,
+        fontWeight: 700,
+        borderRadius: '12px'
     }
-})
+
+}))
 
 
 export default useStyles;
