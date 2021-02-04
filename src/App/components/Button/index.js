3e8a191954b0a81df0c55c070321f73d99f5771a
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-04 16:38:18 +0545

    added Button

---
+++ src/App/components/Button/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+const CustomButton = ({ children }) => {
+  return <Button>{children}</Button>;
+};
+export default CustomButton;
