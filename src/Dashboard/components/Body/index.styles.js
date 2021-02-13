d94a88751d8503cc47451ddbc06fb53e9d0d0a9a
Saurab Gami <saurabgami977@gmail.com>
2021-02-13 14:56:35 +0545

    sidebar styles added and product hover maded limited to screens bigger than 960px

--- src/Dashboard/components/Body/index.styles.js
+++ src/Dashboard/components/Body/index.styles.js
@@ -1,7 +1,19 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
+    sideCategoryContainer: {
+        diaplay: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '332px'
+    },
 
+    links: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        height: '60%'
+    }
 })
 
 export default useStyles
