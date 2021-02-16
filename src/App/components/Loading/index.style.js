be968c4b273a956d32f8a94b1e16243324d50564
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-16 15:33:24 +0545

    style:loading component changed

--- src/App/components/Loading/index.style.js
+++ src/App/components/Loading/index.style.js
@@ -4,6 +4,29 @@ const useStyles = makeStyles((theme) => ({
    backdrop: {
       zIndex: theme.zIndex.drawer + 1,
       color: '#fff',
+      '& .spinner-grow': {
+         display: 'inline-block',
+         width: '2rem',
+         height: '2rem',
+         verticalAlign: 'text-bottom',
+         backgroundColor: 'currentColor',
+         borderRadius: '50%',
+         opacity: 0,
+         '-webkit-animation': 'spinner-grow 1s linear infinite',
+         animation: 'spinner-grow .75s linear infinite',
+      },
+
+      '& .sr-only': {
+         position: 'absolute',
+         width: '1px',
+         height: '1px',
+         padding: 0,
+         margin: '-1px',
+         overflow: ' hidden',
+         clip: 'rect(0,0,0,0)',
+         whiteSpace: 'nowrap',
+         border: 0,
+      },
    },
 }));
 
