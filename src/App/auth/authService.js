835b35d9d45d0e0d6e183bcb7cc18b43f4d50ef2
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-22 15:37:50 +0545

    fix: login reload fix

--- src/App/auth/authService.js
+++ src/App/auth/authService.js
@@ -1,17 +1,17 @@
 import Api from '../services/api';
 
 const auth = {
-  signIn: async (credentials) => {
-    const api = new Api(false);
-    return await api.post('auth/login', credentials);
-  },
-  fetchLoggedInUser: async () => {
-    const api = new Api();
-    return await api.post('/user');
-  },
-  signOut: () => {
-    return true;
-  },
+   signIn: async (credentials) => {
+      const api = new Api(false);
+      return await api.post('auth/login', credentials);
+   },
+   fetchLoggedInUser: async (token) => {
+      const api = new Api(false);
+      return await api.post('/user', { token });
+   },
+   signOut: () => {
+      return true;
+   },
 };
 
 export default auth;
--- src/App/auth/index.js
+++ src/App/auth/index.js
@@ -2,6 +2,8 @@ import React, { createContext, useContext, Component } from 'react';
 import Cookies from 'js-cookie';
 import authService from './authService';
 import Loading from '../components/Loading';
+import Api from '../services/api';
+
 const initialState = {
    isUserLoggedIn: false,
 
@@ -11,15 +13,15 @@ const initialState = {
    loginUser: async (credentials) => {
       try {
          const res = await authService.signIn(credentials);
-         window.location.reload();
+
          return res;
       } catch (err) {
          throw err.message;
       }
    },
-   fetchLoggedInUser: async () => {
+   fetchLoggedInUser: async (token) => {
       try {
-         const res = await authService.fetchLoggedInUser();
+         const res = await authService.fetchLoggedInUser(token);
          return res;
       } catch (err) {
          throw err.message;
@@ -45,18 +47,42 @@ export const AuthContext = createContext(initialState);
 export default class AuthProvider extends Component {
    state = initialState;
 
-   componentDidMount = () => {
+   componentDidMount = async () => {
+      this.setState({
+         setUserData: () => {
+            this.setState({
+               authStatusReported: true,
+               isUserLoggedIn: true,
+            });
+         },
+      });
       const token = Cookies.get('token');
-
       if (token) {
+         await this.verifyToken(token);
+      } else {
+         this.setState({
+            authStatusReported: true,
+            isUserLoggedIn: false,
+         });
+      }
+   };
+
+   verifyToken = async (token) => {
+      const api = new Api(false);
+      try {
+         const res = await api.post('auth/verify', { token });
+         const { setToken } = this.state;
+         setToken(res.token);
          this.setState({
             authStatusReported: true,
             isUserLoggedIn: true,
          });
-      } else {
+      } catch (err) {
          this.setState({
             authStatusReported: true,
+            isUserLoggedIn: false,
          });
+         Cookies.remove('token');
       }
    };
 
