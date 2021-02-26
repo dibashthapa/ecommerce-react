ac1703204258c2c58c44756e048188c7cbc60511
Dibash Thapa <dibashthapa55@gmail.com>
2021-02-27 03:11:53 +0545

    skeleton added

--- src/Dashboard/pages/Dashboard/components/Product/index.js
+++ src/Dashboard/pages/Dashboard/components/Product/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 import AppProduct from '../../../../../App/components/Product';
-import Loading from '../../../../../App/components/Loading';
 import useStyles from './index.style.js';
 import { ProductActions } from '../../store';
 import { connect } from 'react-redux';
 import Pagination from '../../../../../App/components/Pagination';
+import { Skeleton } from '@material-ui/lab';
 const Product = (props) => {
    const classes = useStyles();
 
@@ -17,7 +17,7 @@ const Product = (props) => {
 
    return (
       <Grid container className={classes.root} justify="center" spacing={2}>
-         {props.products?.length <= 0 && (
+         {!props.loading && props.products?.length <= 0 && (
             <img src="/image/404.png" alt="No products found" />
          )}
          <Grid item container xs={10}>
@@ -29,21 +29,25 @@ const Product = (props) => {
                style={{ marginTop: '10px' }}
                spacing={3}
             >
-               {props.loading ? (
-                  <Loading open={true} />
-               ) : (
-                  props.products?.map(({ _id, name, price, imgUrl, description }) => (
-                     <Grid item xs={3} key={_id}>
-                        <AppProduct
-                           img_url={imgUrl}
-                           name={name}
-                           price={price}
-                           description={description}
-                           id={_id}
-                        />
-                     </Grid>
-                  ))
-               )}
+               {props.loading
+                  ? Array.from(new Array(10)).map((item, index) => (
+                       <Grid item xs={3} key={index}>
+                          <Skeleton variant="rect" height={250} />
+                          <Skeleton />
+                          <Skeleton />
+                       </Grid>
+                    ))
+                  : props.products?.map(({ _id, name, price, imgUrl, description }) => (
+                       <Grid item xs={3} key={_id}>
+                          <AppProduct
+                             img_url={imgUrl}
+                             name={name}
+                             price={price}
+                             description={description}
+                             id={_id}
+                          />
+                       </Grid>
+                    ))}
             </Grid>
          </Grid>
          <Pagination totalPage={props.totalProduct.totalPages} />
